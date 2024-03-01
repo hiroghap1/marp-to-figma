e6e14ea5ffe7861dfa4540b37b7155f40525bde3
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2024-03-01 21:29:00 +0900

    feat(ui):フロントでもmarp対応かチェック

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,33 +1,39 @@
 import { render } from '@create-figma-plugin/ui'
-import { h } from 'preact'
+import { h, Fragment } from 'preact'
+import { useState } from 'preact/hooks'
 import '!./output.css'
 import JSX = h.JSX;
 
 function Plugin () {
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleFileChange = (event: JSX.TargetedEvent<HTMLInputElement, Event>) => {
+    setErrorMessage(''); // エラーメッセージをリセット
     const file = event.currentTarget.files?.[0];
     if (!file) {
-      console.error('ファイルが選択されていません。');
+      setErrorMessage('ファイルが選択されていません。');
       return;
     }
 
     const reader = new FileReader();
     reader.onload = function(loadEvent) {
-      // @ts-ignore
-      const content = loadEvent.target.result;
-      // console.log(content);
-      parent.postMessage({ pluginMessage: { type: 'upload-md', content: content } }, '*');
+      const content = loadEvent.target?.result as string;
+      if (content.startsWith('---\nmarp: true\n---')) {
+        parent.postMessage({ pluginMessage: { type: 'upload-md', content } }, '*');
+      } else {
+        setErrorMessage('このファイルはMarp対応のMarkdownフォーマットではありません。');
+      }
     };
     reader.readAsText(file);
   };
 
   return (
-    <div>
-      <h1 class="text-2xl font-bold">
+    <div class="p-4">
+      <h1 class="text-xl font-bold mb-2">
         .mdファイルを読み込んでください
       </h1>
       <input type="file" id="fileInput" accept=".md" onChange={handleFileChange} />
+      {errorMessage && <p class="text-red-500 mt-2">{errorMessage}</p>}
     </div>
   )
 }
