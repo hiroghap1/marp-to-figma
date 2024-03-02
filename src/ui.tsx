0c9666c2cd086b55ca1e54110bf6c7f2b9bdd11f
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2024-03-02 18:40:20 +0900

    feat(ui):Marpの設定ファイルを受け入れるように修正

--- src/ui.tsx
+++ src/ui.tsx
@@ -18,7 +18,35 @@ function Plugin () {
     const reader = new FileReader();
     reader.onload = function(loadEvent) {
       const content = loadEvent.target?.result as string;
-      if (content.startsWith('---\nmarp: true\n---')) {
+      const sections = content.split('---');
+      const headerSection = sections[1]; // 最初の '---' に囲まれたセクション
+      const lines = headerSection.split('\n');
+      const config:{[key: string]: string} = {};
+
+      for (const line of lines) {
+        const [key, rawValue] = line.split(':').map(part => part.trim());
+        if (key && rawValue) {
+          let value: any = rawValue;
+
+          // "true"または"false"の文字列をブール値に変換
+          if (rawValue === "true" || rawValue === "false") {
+            value = rawValue === "true";
+          }
+
+          // 数値の文字列を数値に変換
+          else if (!isNaN(Number(rawValue))) {
+            value = Number(rawValue);
+          }
+
+          // クォーテーションで囲まれた文字列からクォーテーションを削除
+          else if (rawValue.startsWith('"') && rawValue.endsWith('"')) {
+            value = rawValue.slice(1, -1);
+          }
+
+          config[key] = value;
+        }
+      }      console.log(config);
+      if (config['marp']?.replace(/"/g, '') === 'true') {
         parent.postMessage({ pluginMessage: { type: 'upload-md', content } }, '*');
       } else {
         setErrorMessage('このファイルはMarp対応のMarkdownフォーマットではありません。');
